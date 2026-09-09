openwrt/openwrt

commit 3c152904c22fb6754820e36a0b7bd20b90b64dc1

realtek: add fan controller support to D-Link DGS-1210-28MP
The DGS-1210-28MP has a LM63 fan controller connected via i2c of the
RTL8231. The clock line is always low if the property
i2c-gpio,scl-open-drain is not set; with this property, the GPIO pin is
force-drive and the clock works as expected.

The LM63 is not configured by U-Boot, thus only manual fan control is
possible by settings pwm1_enable to "1" and writing the desired values to
pwm1.

The OEM firmware drives the fan from user mode and sets it up like this:

// PWM LUT/value r/w, PWM Clock = 1.4kHz
0x4a 0x28
// Tachometer spinup disabled, spin-up cycles bypassed
0x4b 0x00
// PWM Frequency = default
0x4d 0x17
// PWM Value (28)
0x4c 0x1c
// If > 0 C, use
0x50 0x00
// PWM = 28
0x51 0x1c
// If > 51 C, use
0x52 0x33
// PWM = 44
0x53 0x2e
// Set hysteresis to 100 = default
0x4f 0x03
// Turn on automatic mode and w/p the LUT values
0x4a 0x08

A thread in the OEM firmware polls the ALERT status register for fan
failures.

Unfortunately, the lm63 kernel driver does not perform any initialization
of the chip and it does not support changing some config registers (like
PWM frequency or LUT). Hence, we are stuck with the defaults and need to do
fan control in software.

Signed-off-by: Andreas Böhler <dev@aboehler.at>
Link: https://github.com/openwrt/openwrt/pull/15616
Signed-off-by: Sander Vanheule <sander@svanheule.net>

target/linux/realtek/base-files/sbin/fan_ctrl.sh
@@ -1,0 +1,13 @@
+#!/bin/sh
+
+PSU_TEMP=$(cut -c1-2 /sys/class/hwmon/hwmon0/temp1_input)
+
+FAN_CTRL='/sys/class/hwmon/hwmon0/pwm1'
+
+PSU_THRESH=51000
+
+if [ "$PSU_TEMP" -ge "$PSU_THRESH" ];then
+	echo "250" > $FAN_CTRL
+else
+	echo "156" > $FAN_CTRL
+fi

target/linux/realtek/base-files/etc/uci-defaults/04_dlinkfan
@@ -1,0 +1,26 @@
+#
+# Copyright (C) 2024 openwrt.org
+#
+
+. /lib/functions.sh
+
+board=$(board_name)
+
+case "$board" in
+d-link,dgs-1210-28mp-f)
+	# Enable fan control
+	FAN_CTRL='/sys/class/hwmon/hwmon0'
+	echo 1 > "$FAN_PATH/pwm1_enable"
+
+	# Set fan script execution in crontab
+	grep -s -q fan_ctrl.sh /etc/crontabs/root && exit 0
+
+	echo "# dlink fan script runs every 5 minutes" >> /etc/crontabs/root
+	echo "*/5 * * * * /sbin/fan_ctrl.sh" >> /etc/crontabs/root
+
+	# Execute one time after initial flash (instead of waiting 5 min for cron)
+	/sbin/fan_ctrl.sh
+	;;
+esac
+
+exit 0

target/linux/realtek/dts/rtl8382_d-link_dgs-1210-28mp-f.dts
@@ -9,6 +9,22 @@
 / {
 	compatible = "d-link,dgs-1210-28mp-f", "realtek,rtl8382-soc", "realtek,rtl838x-soc";
 	model = "D-Link DGS-1210-28MP F";
+
+	/* LM63 */
+	i2c-gpio-4 {
+		compatible = "i2c-gpio";
+		sda-gpios = <&gpio1 32 (GPIO_ACTIVE_HIGH | GPIO_OPEN_DRAIN)>;
+		scl-gpios = <&gpio1 31 GPIO_ACTIVE_HIGH>;
+		i2c-gpio,delay-us = <2>;
+		i2c-gpio,scl-open-drain; /* should be replaced by i2c-gpio,scl-has-no-pullup in kernel 6.6 */
+		#address-cells = <1>;
+		#size-cells = <0>;
+
+		lm63@4c {
+			compatible = "national,lm63";
+			reg = <0x4c>;
+		};
+	};
 };
 
 &leds {

target/linux/realtek/image/rtl838x.mk
@@ -71,7 +71,7 @@ define Device/d-link_dgs-1210-28mp-f
   SOC := rtl8382
   DEVICE_MODEL := DGS-1210-28MP
   DEVICE_VARIANT := F
-  DEVICE_PACKAGES += realtek-poe
+  DEVICE_PACKAGES += realtek-poe kmod-hwmon-lm63
 endef
 TARGET_DEVICES += d-link_dgs-1210-28mp-f
 

rockyli/openwrt-XR30

commit c634136c8b52fcee92a8ac69e5eb42f06622a3fa

realtek: fix D-Link DGS-1210-28P/MP fan controller
The fan_ctrl.sh script incorrectly assumed that hwmon0 would always be
the lm63 device. Since the introduction of cpu_thermal for this target,
the initialization order of hwmon devices has changed. This patch
dynamically locates the correct hwmon device used by lm63.

Additionally, the script was reading only the first two characters of
the temperature (truncating millidegrees to degrees Celsius, assuming it
never dropped below 10°C) but subsequently compared it against a
threshold defined in millidegrees (51000). This caused the comparison to
always fail. This patch removes the truncation, ensuring both values are
properly compared in millidegrees.

Note: The main branch uses a different approach for this fix
(d3290efad9a9).

Fixes: 3c152904c22f ("realtek: add fan controller support to D-Link DGS-1210-28MP")
Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

target/linux/realtek/base-files/sbin/fan_ctrl.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-PSU_TEMP=$(cut -c1-2 /sys/class/hwmon/hwmon0/temp1_input)
+LM63_HWMON=$(grep -Fx -l lm63 /sys/class/hwmon/hwmon*/name) || exit 0
+LM63_HWMON=${LM63_HWMON%/name}
 
-FAN_CTRL='/sys/class/hwmon/hwmon0/pwm1'
+PSU_TEMP=$(cat "$LM63_HWMON/temp1_input")
+FAN_CTRL="$LM63_HWMON/pwm1"
 
 PSU_THRESH=51000
 